0dd430c3063dc48730886f852359ad3562ec4b2d
RHX <ryanjhendrix@gmail.com>
2015-10-13 22:10:10 -0700

    modified index.html, js/helper.js, js/resumeBulder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,10 @@
-$("#main").append("Ryan Hendrix");
+$("#header").append("Ryan Hendrix");
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%",  role);
+
+$("#header").prepend(formattedRole);
+
