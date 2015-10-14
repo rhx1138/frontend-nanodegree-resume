64bc36b50fccc5c3a589e4d745533ab8361e4c61
RHX <ryanjhendrix@gmail.com>
2015-10-14 11:08:17 -0700

    modified js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,26 @@
-$("#header").append("Ryan Hendrix");
 
-var formattedName = HTMLheaderName.replace("%data%", name);
+var bio = {
+  "name" : "Ryan Hendrix",
+  "role" : "Web Developer,",
+    "mobile" : "xxx-xxx-xxxx",
+    "email" : "ryanjhendrix@gmail.com",
+    "github" : "rhx1138",
+    "twitter" : "None",
+    "location" : "San Rafael, CA",
+  };
 
-$("#header").append(formattedName);
 
-var formattedRole = HTMLheaderRole.replace("%data%",  role);
+// $("#header").append("Ryan Hendrix");
 
-$("#header").prepend(formattedRole);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+console.log(formattedName);
+
+$("#header").prepend(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+console.log(formattedRole);
+
+$("#header").append(formattedRole);
 
