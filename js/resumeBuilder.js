05fb9624697a9641a05bbb67d494889270a3fd83
RHX <ryanjhendrix@gmail.com>
2015-10-23 09:25:15 -0700

    modified index.html, modified js/res.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,26 +1,64 @@
 
+
+
+
 var bio = {
-  "name" : "Ryan Hendrix",
-  "role" : "Web Developer,",
-    "mobile" : "xxx-xxx-xxxx",
-    "email" : "ryanjhendrix@gmail.com",
-    "github" : "rhx1138",
-    "twitter" : "None",
-    "location" : "San Rafael, CA",
-  };
+	"name" : "Ryan Hendrix",
+	"role" : "Web Developer",
+	"contacts": {
+		"mobile" : "xxx-xxx-xxxx",
+		"email" : "ryanjhendrix@gmail.com",
+		"github" : "rhx1138",
+		"twitter" : "None",
+		"location" : "San Rafael, CA",
+	},
+	"welcomeMessage": "lorem ipsoum dolor sit amet etc etc etc.",
+	"skills" : ["programming", "teaching", "JS"],
+	"bioPic": ""
 
+	};
 
-// $("#header").append("Ryan Hendrix");
+var education = {
+	"schools": [
+		{
+			"name": "Monterey Institute of International Studies",
+			"city": "Monterey, CA",
+			"degree": "MA",
+			"major": "International Policy Studies"
+		},
+		{
+			"name": "Indiana State University",
+			"city": "Indiana",
+			"degree": "BS",
+			"major": "Graphic Design"
+		}
+	]
+}
+
+var work = {};
+work.position = "Industry Analyst";
+work.employer = "Frost and Sullivan";
+work.years = 3;
+
+//
+var education = {};
+education["name"] = "Monterey Institute of International Studies";
+education ["city"] = "Monterey, CA";
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
-
-console.log(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContacts = HTMLheaderContacts.replace(%data%,bio.role);
+var formattedWorkExperience; //HTML
 
 $("#header").prepend(formattedName);
-
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-console.log(formattedRole);
-
 $("#header").append(formattedRole);
+$("#header") 
+
+//HTML 
+$("topContacts").append(bio.contacts);
+$("#workExperience").append(bio.skill);
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
 
